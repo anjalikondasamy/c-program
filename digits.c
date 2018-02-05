0e6347302ce3afd58f98e0e2ea1e104b36e2bf71
anjalikondasamy <35995743+anjalikondasamy@users.noreply.github.com>
2018-02-05 16:45:34 +0530

    cpgm
    good

---
+++ digits.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,i,sum;
+float integer;
+clrscr();
+
+printf("\n Enter the number of digits");
+scanf("%d',&digits);
+
+for(i=0;i<=1;i++)
+{
+sum=n+i;
+}
+
+printf("\n Sum of digits of integer:");
+scanf("%d");
+return 0;
+}
